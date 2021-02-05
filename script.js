e2ffb34d07facc165a83ae7ef53d97efee6639eb
Ashley-Croucamp <76824476+Ashley-Croucamp@users.noreply.github.com>
2021-02-05 13:38:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,17 +20,85 @@ const addInfo = (event) => {
   attendanceInfo.push(Info);
   document.querySelector('form').reset();
   
-  console.log('added', {attendanceInfo} );
+  document.getElementById('getInfo').innerHTML = Info.name;
+  
+  //console.log('added', {attendanceInfo} );
+  
+  localStorage.setItem('Info', JSON.stringify(Info));
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+if (document.getElementById('addAttendanceBtn') != null) {
   document.getElementById('addAttendanceBtn').addEventListener('click', addInfo);
-});
+}
+
+var AutoSave = (function(){
+
+	var timer = null;
+
+	function getEditor(){
+
+		var elems = document.getElementsByTagName("textarea")
+		if (elems.length <= 0)
+			return null;
+
+		return elems[0];
+	}
 
 
+	function save(){
+
+		var editor = getEditor(); 
+                if (editor) {
+		    localStorage.setItem("AUTOSAVE_" + document.location, editor.value )
+                }
+
+	}
+
+
+	function restore(){
+
+		var saved = localStorage.getItem("AUTOSAVE_" + document.location)
+		var editor = getEditor();
+		if (saved && editor){
+
+			editor.value = saved; 
+		}
+	}
+
+	return { 
+
+		start: function(){
+
+			var editor = getEditor(); 
+
+		 
+			if (editor.value.length <= 0)
+				restore();
+
+			if (timer != null){
+				clearInterval(timer);
+				timer = null;
+			}
+
+			timer = setInterval(save, 5000);
+
+
+		},
+
+		stop: function(){
+
+			if (timer){ 
+				clearInterval(timer);
+				timer = null;
+			}
+
+		}
+	}
+
+}())
 
 /**
 TODO
  *    Change date to SA standard.
- *    Fix line 56. Getting TypeError, needs to be fixed and error free or use first one that has been commente out.
+ *    Loop through object and display in html as a list.
 **/
