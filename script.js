a2a12febcd4d5750701dbe405be22f0781d260ab
Ashley-Croucamp <76824476+Ashley-Croucamp@users.noreply.github.com>
2021-02-05 21:25:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ if(document.getElementById('showCurrentDate') != null) {
   document.getElementById('showCurrentDate').innerHTML = localDate;
 }
 
+//let time = new Date().getTime();
+
 // Store names and email of people who attends the meeting as an object after it has been added
 let attendanceInfo = [];
 
@@ -13,26 +15,42 @@ const addInfo = (event) => {
   event.preventDefault();
 
   let Info = {
-    name: document.getElementById('attendanceName').value,
-    email: document.getElementById('attendanceEmail').value
+    Name: document.getElementById('attendanceName').value,
+    Email: document.getElementById('attendanceEmail').value,
+    Time: new Date().getTime()
   };
   
   attendanceInfo.push(Info);
   document.querySelector('form').reset();
   
-  document.getElementById('getInfo').innerHTML = Info.name;
+  // Creates a new button and line break for Info.name and appends every entry as a button
+  var newDomBtn = document.createElement('button');
+  newDomBtn.innerHTML = Info.Name;
+  var newLineBreak = document.createElement('br');
+  document.getElementById('getInfo').appendChild(newDomBtn);
+  document.getElementById('getInfo').appendChild(newLineBreak);
   
-  //console.log('added', {attendanceInfo} );
+  // DOM buttons style
+  newDomBtn.style.backgroundColor = '#A7C7E7';
+  newDomBtn.style.color = 'black';
+  newDomBtn.style.fontSize = '13px';
+  newDomBtn.style.textDecoration = 'none';
+  newDomBtn.style.textAlign = 'center';
+  newDomBtn.style.padding = '12px 29px';
+  newDomBtn.style.border = 'none';
+  newDomBtn.style.margin = '3px';
+  newDomBtn.style.borderRadius = '2px';
   
-  localStorage.setItem('Info', JSON.stringify(Info));
 };
 
 if (document.getElementById('addAttendanceBtn') != null) {
-	document.getElementById('addAttendanceBtn').addEventListener('click', addInfo);
+  document.getElementById('addAttendanceBtn').addEventListener('click', addInfo);
 }
+
+
 
 /**
 TODO
  *    Change date to SA standard.
- *    Loop through object and display in html as a list.
+ *    Return current users name.
 **/
