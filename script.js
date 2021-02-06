4a9318657d1d84d94da2c85223d858501dce3098
Ashley-Croucamp <76824476+Ashley-Croucamp@users.noreply.github.com>
2021-02-06 19:50:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ if(document.getElementById('showCurrentDate') != null) {
   document.getElementById('showCurrentDate').innerHTML = localDate;
 }
 
-//let time = new Date().getTime();
-
 // Store names and email of people who attends the meeting as an object after it has been added
 let attendanceInfo = [];
 
-const addInfo = (event) => {
+const addInfo = (event) => {  
   
   event.preventDefault();
 
@@ -19,7 +17,6 @@ const addInfo = (event) => {
     Email: document.getElementById('attendanceEmail').value,
     Time: new Date().getTime()
   };
-  
   attendanceInfo.push(Info);
   document.querySelector('form').reset();
   
@@ -30,7 +27,7 @@ const addInfo = (event) => {
   document.getElementById('getInfo').appendChild(newDomBtn);
   document.getElementById('getInfo').appendChild(newLineBreak);
   
-  // DOM buttons style
+  // DOM button style
   newDomBtn.style.backgroundColor = '#A7C7E7';
   newDomBtn.style.color = 'black';
   newDomBtn.style.fontSize = '13px';
@@ -39,14 +36,40 @@ const addInfo = (event) => {
   newDomBtn.style.padding = '12px 29px';
   newDomBtn.style.border = 'none';
   newDomBtn.style.margin = '3px';
-  newDomBtn.style.borderRadius = '2px';
-  
+  newDomBtn.style.borderRadius = 'px';
+
 };
 
+// Creates an event listener to add more people attending the meeting
 if (document.getElementById('addAttendanceBtn') != null) {
   document.getElementById('addAttendanceBtn').addEventListener('click', addInfo);
 }
 
+// Creates the current user's button to attend the meeting
+var currentUser = prompt('Please enter your name');
+if (currentUser != null) {
+  if (document.getElementById('currentUserName') != null) {
+    var getCurrentUserName = document.getElementById('currentUserName').value = currentUser;
+  }
+}
+
+var currentUserBtn = document.createElement('button');
+currentUserBtn.innerHTML = getCurrentUserName;
+var firstLineBreak = document.createElement('br');
+document.getElementById('getInfo').appendChild(currentUserBtn);
+document.getElementById('getInfo').appendChild(firstLineBreak);
+
+currentUserBtn.style.backgroundColor = '#A7C7E7';
+currentUserBtn.style.color = 'black';
+currentUserBtn.style.fontSize = '13px';
+currentUserBtn.style.textDecoration = 'none';
+currentUserBtn.style.textAlign = 'center';
+currentUserBtn.style.padding = '12px 29px';
+currentUserBtn.style.border = 'none';
+currentUserBtn.style.margin = '3px';
+currentUserBtn.style.borderRadius = 'px';
+
+// Save meeing notes that has been typed
 
 
 /**
